28891fa04afa2e280df69a78c15cebd3fe222279
Md Hanif Seddiqui <mhs62@cam.ac.uk>
2025-09-10 11:20:36 +0100

    ckan-dcat-docker: create config.py that contains api_key, ckan_url etc.

---
+++ src/catalog/config.py
@@ -0,0 +1,6 @@
+# ----------------------------
+# CONFIGURATION
+# ----------------------------
+API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJaMElramVFSWFMQ2ZkYUlyeEltMll3eVlOZGpwekdqcG5jaTAzT2JLNGVVIiwiaWF0IjoxNzU3NDM3Nzg1fQ.mUijPSZVKwaVntmfGeqd0wr7qfcDpsIQqgQRpacoR2o"
+CKAN_URL = "https://localhost:8443"
+VERIFY_SSL = False  # set True if you have valid cert
